18ec0a7cbaec2ad9912a86dfbbd95b0b79dfeddf
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2022-04-07 12:38:50 -0300

    Preparing the layout LeftSide
    prepring the base for the leftside (pre stylization)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import poweredImage from "./assets/powered.png";
 
 const App = () => {
+  const [height, setHeight] = useState<number>(0);
+  const [weight, setWeight] = useState<number>(0);
+
+  const handleCalculation = () => {
+    if (height !== 0 && weight !== 0 && height && weight) {
+    } else {
+      alert("Preencha todos os campos");
+    }
+  };
   return (
     <div className={styles.main}>
       <header>
         <div className={styles.headerContainer}>
-          <img src={poweredImage} alt="" width={150}/>
+          <img src={poweredImage} alt="" width={150} />
         </div>
       </header>
       <div className={styles.container}>
-          <div className={styles.leftSide}>---</div>
-          <div className={styles.rightSide}>+++</div>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+          <p>
+            IMC é a sigla para Índice de Massa Corpórea,parâmero adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada
+            pessoa
+          </p>
+
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex 1.5(em metros)"
+            value={height > 0 ? height : ""}
+            onChange={(e) => setHeight(parseFloat(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex 60.3 (em quilos)"
+            value={weight > 0 ? weight : ""}
+            onChange={(e) => setWeight(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculation}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>+++</div>
       </div>
     </div>
   );
